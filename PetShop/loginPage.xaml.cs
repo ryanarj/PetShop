6052d5964713d331ea7f71b1348ff68de6d8306d
Ryan Arjun <karshan813@gmail.com>
2018-11-04 11:28:42 -0500

    Initial commit

---
+++ PetShop/loginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PetShop
+{
+    /// <summary>
+    /// Interaction logic for loginPage.xaml
+    /// </summary>
+    public partial class loginPage : Page
+    {
+        public loginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void registerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new registerPage();
+        }
+    }
+}
